d7a401d11ee6946abcae39662b5de8317539bc2d
elyas20 <iliasdahi@gmail.com>
2018-02-01 12:16:34 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Streaming.MixProject do
       app: :streaming,
       version: "0.1.0",
       elixir: "~> 1.6",
-      escript: escript,
+      escript: escript(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
